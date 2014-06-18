97f8df3e1e35e44e2a53290b1f154db1a15197d0
RobThree <rob@devcorner.nl>
2014-06-18 21:29:56 +0200

    * More unittests * Swapped target<->excpected in asserts (d'oh... I *always* mess this up...)

--- NGeoNamesTests/ParserTests.cs
+++ NGeoNamesTests/ParserTests.cs
@@ -34,25 +34,25 @@ namespace NGeoNamesTests
             var target = GeoFileReader.ReadAdmin1Codes(@"testdata\test_admin1CodesASCII.txt").ToArray();
             Assert.AreEqual(4, target.Length);
 
-            Assert.AreEqual(target[0].Code, "CF.04");
-            Assert.AreEqual(target[0].Name, "Mambéré-Kadéï");
-            Assert.AreEqual(target[0].NameASCII, "Mambere-Kadei");
-            Assert.AreEqual(target[0].GeoNameId, 2386161);
+            Assert.AreEqual("CF.04", target[0].Code);
+            Assert.AreEqual("Mambéré-Kadéï", target[0].Name);
+            Assert.AreEqual("Mambere-Kadei", target[0].NameASCII);
+            Assert.AreEqual(2386161, target[0].GeoNameId);
 
-            Assert.AreEqual(target[1].Code, "This.is.a.VERY.LONG.ID");
-            Assert.AreEqual(target[1].Name, "Iğdır");
-            Assert.AreEqual(target[1].NameASCII, "Igdir");
-            Assert.AreEqual(target[1].GeoNameId, 0);
+            Assert.AreEqual("This.is.a.VERY.LONG.ID", target[1].Code);
+            Assert.AreEqual("Iğdır", target[1].Name);
+            Assert.AreEqual("Igdir", target[1].NameASCII);
+            Assert.AreEqual(0, target[1].GeoNameId);
 
-            Assert.AreEqual(target[2].Code, "TR.80");
-            Assert.AreEqual(target[2].Name, "Şırnak");
-            Assert.AreEqual(target[2].NameASCII, "Sirnak");
-            Assert.AreEqual(target[2].GeoNameId, 443189);
+            Assert.AreEqual("TR.80", target[2].Code);
+            Assert.AreEqual("Şırnak", target[2].Name);
+            Assert.AreEqual("Sirnak", target[2].NameASCII);
+            Assert.AreEqual(443189, target[2].GeoNameId);
 
-            Assert.AreEqual(target[3].Code, "UA.26");
-            Assert.AreEqual(target[3].Name, "Zaporiz’ka Oblast’");
-            Assert.AreEqual(target[3].NameASCII, "Zaporiz'ka Oblast'");
-            Assert.AreEqual(target[3].GeoNameId, 687699);
+            Assert.AreEqual("UA.26", target[3].Code);
+            Assert.AreEqual("Zaporiz’ka Oblast’", target[3].Name);
+            Assert.AreEqual("Zaporiz'ka Oblast'", target[3].NameASCII);
+            Assert.AreEqual(687699, target[3].GeoNameId);
         }
 
         [TestMethod]
@@ -61,15 +61,15 @@ namespace NGeoNamesTests
             var target = GeoFileReader.ReadAdmin2Codes(@"testdata\test_admin2Codes.txt").ToArray();
             Assert.AreEqual(2, target.Length);
 
-            Assert.AreEqual(target[0].Code, "AF.01.7052666");
-            Assert.AreEqual(target[0].Name, "Darwāz-e Bālā");
-            Assert.AreEqual(target[0].NameASCII, "Darwaz-e Bala");
-            Assert.AreEqual(target[0].GeoNameId, 7052666);
+            Assert.AreEqual("AF.01.7052666", target[0].Code);
+            Assert.AreEqual("Darwāz-e Bālā", target[0].Name);
+            Assert.AreEqual("Darwaz-e Bala", target[0].NameASCII);
+            Assert.AreEqual(7052666, target[0].GeoNameId);
 
-            Assert.AreEqual(target[1].Code, "CA.10.11");
-            Assert.AreEqual(target[1].Name, "Gaspésie-Îles-de-la-Madeleine");
-            Assert.AreEqual(target[1].NameASCII, "Gaspesie-Iles-de-la-Madeleine");
-            Assert.AreEqual(target[1].GeoNameId, 0);
+            Assert.AreEqual("CA.10.11", target[1].Code);
+            Assert.AreEqual("Gaspésie-Îles-de-la-Madeleine", target[1].Name);
+            Assert.AreEqual("Gaspesie-Iles-de-la-Madeleine", target[1].NameASCII);
+            Assert.AreEqual(0, target[1].GeoNameId);
         }
 
         [TestMethod]
@@ -77,7 +77,7 @@ namespace NGeoNamesTests
         {
             var target = new GeoFileReader().ReadRecords<CustomEntity>(@"testdata\test_custom.txt", new CustomParser()).ToArray();
 
-            Assert.AreEqual(target.Length, 2);
+            Assert.AreEqual(2, target.Length);
             CollectionAssert.AreEqual(target[0].Data, target[1].Data);
         }
 
@@ -87,12 +87,12 @@ namespace NGeoNamesTests
             //Could use ANY entity
             var target1 = new GeoFileReader().ReadRecords<CustomEntity>(@"testdata\emptyfile.txt", new CustomParser()).ToArray();
 
-            Assert.AreEqual(target1.Length, 0);
+            Assert.AreEqual(0, target1.Length);
 
             //Here's a second one
             var target2 = GeoFileReader.ReadExtendedGeoNames(@"testdata\emptyfile.txt").ToArray();
 
-            Assert.AreEqual(target2.Length, 0);
+            Assert.AreEqual(0, target2.Length);
         }
 
         [TestMethod]
@@ -100,7 +100,7 @@ namespace NGeoNamesTests
         {
             var target = GeoFileReader.ReadCountryInfo(@"testdata\countryInfo_gzip_compat.txt.gz").ToArray();
 
-            Assert.AreEqual(target.Length, 2);
+            Assert.AreEqual(2, target.Length);
         }
 
         [TestMethod]
@@ -112,12 +112,21 @@ namespace NGeoNamesTests
         }
 
         [TestMethod]
-        public void FileReader_CanReadBuiltInsCorrectly()
+        public void FileReader_CanReadBuiltInContinentsCorrectly()
         {
-            var target_continents = GeoFileReader.ReadBuiltInContinents().ToArray();
-            var target_featureclasses = GeoFileReader.ReadBuiltInFeatureClasses().ToArray();
+            var target = GeoFileReader.ReadBuiltInContinents().ToArray();
 
+            Assert.AreEqual(7, target.Length);
+            CollectionAssert.AreEqual(target.OrderBy(c => c.Code).Select(c => c.Code).ToArray(), new[] { "AF", "AN", "AS", "EU", "NA", "OC", "SA" });
+        }
 
+        [TestMethod]
+        public void FileReader_CanReadBuiltInFeatureClassesCorrectly()
+        {
+            var target = GeoFileReader.ReadBuiltInFeatureClasses().ToArray();
+
+            Assert.AreEqual(9, target.Length);
+            CollectionAssert.AreEqual(target.OrderBy(c => c.Class).Select(c => c.Class).ToArray(), new[] { "A", "H", "L", "P", "R", "S", "T", "U", "V" });
         }
 
         private class CustomParser : IParser<CustomEntity>
